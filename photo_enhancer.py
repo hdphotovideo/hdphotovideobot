8c47f82286f7a1465f565e1124488865a730dd0f
hdphotovideo <fdre1231231@gmail.com>
2025-07-07 12:07:47 +0530

    Update photo_enhancer.py

--- photo_enhancer.py
+++ photo_enhancer.py
@@ -1,11 +1,10 @@
-from gfpgan import GFPGANer
-import cv2, os
-
-gfpganer = GFPGANer(model_path="GFPGANv1.3.pth", upscale=2, bg_upsampler=None)
+import cv2
+import os
 
 def enhance_photo(in_file):
     img = cv2.imread(in_file)
-    _, _, restored = gfpganer.enhance(img, has_aligned=False, only_center_face=False)
+    height, width = img.shape[:2]
+    upscaled = cv2.resize(img, (width*2, height*2), interpolation=cv2.INTER_CUBIC)
     out = in_file + "_hd.jpg"
-    cv2.imwrite(out, restored)
+    cv2.imwrite(out, upscaled)
     return out
